deed5c5b117388dde0f90f3bbb198c8eef07353b
Mani <manigonzalez82@gmail.com>
2020-01-23 08:24:23 +0800

    Code cleaning and added form clear after submit.

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.js")[env];
-var db = {};
+let fs = require("fs");
+let path = require("path");
+let Sequelize = require("sequelize");
+let basename = path.basename(module.filename);
+let env = process.env.NODE_ENV || "development";
+let config = require(__dirname + "/../config/config.js")[env];
+let db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
@@ -20,17 +20,17 @@ if (config.use_env_variable) {
 }
 
 fs.readdirSync(__dirname)
-  .filter(function(file) {
+  .filter(function (file) {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
+  .forEach(function (file) {
+    let model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(function (modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
