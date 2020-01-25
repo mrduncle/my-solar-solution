ab8de37e120dd99dc6c0526857270fd97ea1d3f3
mrduncle1 <dun__can@hotmail.com>
2020-01-25 08:07:11 +0800

    savings calculation stored procedure complete

---
+++ sp_calc_savings.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `calc_savings`(
+	IN locn VARCHAR(45)
+    ,IN solarea DECIMAL(3,1)
+    ,OUT dolls_yr DECIMAL(6,2)
+)
+BEGIN
+    
+    SET @locin = (SELECT
+				      id
+				  FROM tbllocation
+                  WHERE location = locn);
+	
+    SET dolls_yr = (SELECT
+					    SUM(radiation) * 0.18 * solarea / 1000 * 0.24
+	  				FROM tblradiation
+	  				WHERE locationid = @locin);
+    
+END$$
+DELIMITER ;
